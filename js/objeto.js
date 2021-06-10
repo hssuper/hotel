effd532676250473c027a3404ecad01c34d97178
hssuper <higorsouza2306@gmail.com>
2021-06-10 11:53:47 -0300

    hj foi bom

--- js/objeto.js
+++ js/objeto.js
@@ -15,4 +15,10 @@ quartos.textContent = hotel.totalQuartos;
 var reserva = document.getElementById('reservados');
 reserva.textContent = hotel.reservados;
 
-var disponivel   
+
+
+
+
+var nomeMes= ['jan','fev','mar','abr','mai','jun'] ;
+nomeMes.textContent = d.nomeMes;
+
