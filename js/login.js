7f54880f8b3656b3c45a9e98903f25911747bd7d
hssuper <higorsouza2306@gmail.com>
2021-06-17 11:26:26 -0300

    limpo

--- js/login.js
+++ js/login.js
@@ -1,8 +1,10 @@
-var exp = new RegExp = /[().]/g; 
+var exp =  /[().-]/g; 
 
 function limpaNumero(){
     var limpaNumero = tel;
-var tel = document.getElementById('tel');
-
+var tel = document.getElementById('tel').value;
 var limpo = tel.replace(exp,'');
+limpo = tel.value;
+limpo.replace(exp,'');
+tel.value = limpo.replace(exp,'');
 }
